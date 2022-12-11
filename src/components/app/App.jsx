d3425f88ba784964be0ad0bb5691af7bcfe898c8
P-Kempf <paulonwein@gmail.com>
2022-12-11 14:19:10 +0100

    opcion de modo oscuro agregado

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Personaje from '../Personaje'
+import useLocalStorage from 'use-local-storage'
+import Personaje from '../Personaje';
+function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
-function App () {
-  return (<>
-    <Personaje />
-  </>
+  const switchTheme =()=>{
+    const newTheme = theme === "light" ? "dark" : "light"
+    setTheme(newTheme)
+  }
+  return (
+    <div className='App' data-theme={theme}>
+      <span>Cambiar entre modo noche y modo dia</span>
+      <button onClick={switchTheme}>Cambia a modo {theme == "light" ? "Noche" : "Día"}</button>
+      <Personaje/>      
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 import './i18n'
 import { ContextoUsuarioProvider } from "./contexto/contextoUsuario"
 
