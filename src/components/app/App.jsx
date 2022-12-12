c08d998867e59d747ecdca4ad2406bc927427423
idavid80 <dominguezbueno.david@gmail.com>
2022-12-12 20:33:44 +0100

    paginacion segun figma sin terminar

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,14 @@
 //import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Personaje from '../Personaje'
+//import Personaje from '../Personaje'
 import Registrarse from '../../firebase/registrarse'
 //import RegistroAnonimo from '../../firebase/registroAnonimo'
+import Destacados from '../Destacados'
 
 function App () {
   return (<>
     <Registrarse />
-    <Personaje />
+    <Destacados />
   </>
   )
 }
