aee48605b160038ad146ea3b8c5e5137f2f3fd3b
franalvarez88 <franalvarez88@gmail.com>
2022-12-14 23:35:51 +0100

    maquetacion

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+
+    return (
+        <div className="container my-5 bg-success">
+            <footer className="page-footer font-small blue">
+                <div className="footer-copyright text-center py-3">"Data provided by Marvel. © 2014 Marvel"
+                </div>
+            </footer>
+
+        </div>
+    );
+}
+
+export default Footer;
--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import './feed.css'
 import { obtenerPersonajes } from '../../services/obtenerPersonaje'
 import { Container, Image, Button } from 'react-bootstrap'
+import { AiFillStar } from 'react-icons/ai'
+import { BsArrowLeftShort } from 'react-icons/bs'
+import { FaRegUserCircle } from 'react-icons/fa'
 
 function Feed() {
   const { personajesData, pagina, setPagina } = obtenerPersonajes()
@@ -10,20 +14,36 @@ function Feed() {
   }
 
   return (
-    <Container>
-      {personajesData.map(personaje => (
-        <Container
-          key={personaje.Id}
-          className='d-flex flex-column justify-content-center align-items-center'
-          fluid
-        >
-          <Image
-            className='vw-100'
-            src={`${personaje.thumbnail.path}.${personaje.thumbnail.extension}`}
-            alt={`${personaje.name} imagen`}
-          />
-          <h1>{personaje.name}</h1>
-          <Button
+    <Container >
+      <div className='contenedor_iconos'>
+      <span className='span_izquierda'> <BsArrowLeftShort /></span>
+      <span className='span_derecha'> <FaRegUserCircle /></span>
+      </div>
+      <div className='contenedor_buscar'>
+        <input type="text" placeholder='Buscar...'/>
+      </div>
+      <Container className='d-flex  flex-wrap' >
+
+        {personajesData.map(personaje => (
+          <Container id='contenedor_personaje'
+            key={personaje.Id}
+            className='d-flex flex-column justify-content-center text-white'
+          >
+            <Image
+              className=''
+              src={`${personaje.thumbnail.path}.${personaje.thumbnail.extension}`}
+              alt={`${personaje.name} imagen`}
+            />
+            <div>
+              <div className='contenedor_nombre'>
+                <p>{personaje.name}</p>
+              </div>
+              <div className='contenedor_valoracion'>
+                <p><span><AiFillStar /></span> 4.9</p>
+              </div>
+            </div>
+
+            {/*    <Button
             onClick={() =>
               window.open(
                 personaje.urls.filter(obj => obj.type === 'detail')[0].url,
@@ -32,10 +52,11 @@ function Feed() {
             }
           >
             Ver personaje en Marvel
-          </Button>
-        </Container>
-      ))}
-      <Button onClick={pasarPagina}> Pasar pagina</Button>
+          </Button> */}
+          </Container>
+        ))}
+        <Button onClick={pasarPagina}> Pasar pagina</Button>
+      </Container>
     </Container>
   )
 }
