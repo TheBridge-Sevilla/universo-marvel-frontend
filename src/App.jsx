7a1ad89b9cf79035f06125fc4bd90061af3cb868
xaxi <francisco.perez@thebridgeschool.es>
2022-12-15 19:12:52 +0100

    Integrado en APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import useLocalStorage from 'use-local-storage'
+import IniciarSesion from './pages/login/IniciarSesion'
+import Intro from './pages/intro/Intro'
+import Feed from './pages/feed/Feed'
+function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  )
 
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
+  return (
+    <div className='App' data-theme={theme}>
+    <IniciarSesion />
+    <Feed/>
+      {/*       <button onClick={switchTheme}>Cambia a modo {theme == "light" ? "Noche" : "Día"}</button> */}
+    </div>
+  )
+}
+
+export default App
