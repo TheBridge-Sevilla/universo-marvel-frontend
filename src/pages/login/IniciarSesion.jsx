55d59615f5e41ecb56f0d264a22c85f30c58bfe1
P-Kempf <paulonwein@gmail.com>
2022-12-15 21:21:01 +0100

    arreglado conflicto con rama inicio-sesion

--- src/pages/login/IniciarSesion.jsx
+++ src/pages/login/IniciarSesion.jsx
@@ -9,20 +9,15 @@ import {
 import { useTranslation } from 'react-i18next'
 import { Container } from 'react-bootstrap'
 import { useSignWithG } from './../../hooks/useSignWithG'
-
-import { iniciarSesionAnonima } from '../../services/firebase/iniciarSesionAnonima'
 import { useContextoUsuario } from '../../context/contextoUsuario'
 import { useState } from 'react'
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../services/firebase/firebase'
 
-
 function IniciarSesion() {
   const { t } = useTranslation()
   const {  usuario,setUsuario } = useContextoUsuario()
   const { iniciarSesionConG } = useSignWithG()
-
-  const { inicioAnonimo } = iniciarSesionAnonima()
   const [email, setEmail] = useState('')
   const [contraseña, setContraseña] = useState('')
 
@@ -101,45 +96,6 @@ function IniciarSesion() {
         <h5 className='mt-5'>
           <u>{t('crear-cuenta')}</u>
         </h5>
-
-  const { inicioAnonimo } = sesionInvitado()
-
-  return (
-    <Container className='h-100 d-flex flex-column justify-content-center' fluid>
-      <h1 className='my-5'>{t('iniciar-sesion')}</h1>
-      <FormGroup className='mt-5 mx-5 d-flex flex-column justify-content-center'>
-        <TextField
-          name='nombre'
-          label={t('nombre')}
-          type='text'
-          className='mb-4'
-        />
-        <TextField
-          name='contraseña'
-          label={t('contraseña')}
-          type='password'
-          className='mb-2'
-        />
-        <Container className='d-flex justify-content-between align-items-center mb-5'>
-          <FormControlLabel control={<Checkbox />} label={t('recordar')} />
-          <a>{t('contraseña-olvidada')}</a>
-        </Container>
-        <Button className='mb-3' size='large'>
-          {t('continuar')}
-        </Button>
-      </FormGroup>
-      <Container className='d-flex justify-content-center mt-5'>
-        <Google
-          size={40}
-          className='zoom-animation mx-4'
-          onClick={() => iniciarSesionConG()}
-        />
-        <DoorOpen
-          size={40}
-          className='zoom-animation mx-4'
-          onClick={() => inicioAnonimo()}
-        />
-
       </Container>
     </div>
   )
