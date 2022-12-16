092ab22fd1d4c46504ae17cb549c07453c14262a
xaxipiruli <frapercan1@alum.us.es>
2022-12-16 16:51:38 +0100

    slider infinito mas o menos funcionando

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -2,46 +2,27 @@ import { obtenerPersonajes } from '../../services/obtenerPersonaje'
 import { Container, Image, Button, Form } from 'react-bootstrap'
 import PaginacionPersonajes from '../../components/PaginacionPersonajes'
 import { React, useRef, useState, useEffect } from 'react'
-import Spinner from 'react-bootstrap/Spinner'
 import Personajes from '../../components/personajes/Personajes'
 
-
 function Feed() {
-  const [pagina, setPagina] = useState(1)
-  const [personajes, setPersonajes] = useState()
   const [filtro, setFiltro] = useState('')
 
-  useEffect(() => {
-    obtenerPersonajes(setPersonajes, pagina, filtro)
-  }, [pagina, filtro])
-
-  if (!personajes) {
-    //Componentizar?
-    return <span><Spinner animation='border' />Cargando personajes</span> 
-  } else {
-    return (
-      <Container>
-        <PaginacionPersonajes
-          personajes={personajes}
-          pagina={pagina}
-          setPagina={setPagina}
+  return (
+    <Container>
+      {/* Mejor en un componente */}
+      <Form className='h-100 d-flex flex-column justify-content-center'>
+        <Form.Control
+          type='text'
+          size='lg'
+          placeholder='Filtro'
+          onChange={event => {
+            setFiltro(event.target.value)
+          }}
         />
-        {/* Mejor en un componente */}
-        <Form className='h-100 d-flex flex-column justify-content-center'>
-          <Form.Control
-            type='text'
-            size='lg'
-            placeholder='Filtro'
-            onChange={event => {
-              setFiltro(event.target.value)
-              setPagina(1)
-            }}
-          />
-        </Form>
-        <Personajes personajes={personajes}/>
-      </Container>
-    )
-  }
+      </Form>
+      <Personajes filtro={filtro} />
+    </Container>
+  )
 }
 
 export default Feed
--- src/services/obtenerPersonaje.jsx
+++ src/services/obtenerPersonaje.jsx
@@ -1,12 +1,13 @@
 export function obtenerPersonajes(setPersonajes,pagina, filtro) {
   
-  const url = `${import.meta.env.VITE_BASE_URL}/personajes?page=${pagina}&limit=${import.meta.env.VITE_PAGINATION_LIMIT}&filter=${filtro}`
+  
 
   fetchApi()
 
   async function fetchApi() {
     const data = await fetch(url)
     const json = await data.json()
+    console.log(json)
     setPersonajes(json)
   }
 
