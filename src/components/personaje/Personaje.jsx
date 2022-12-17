045ab5248c7cdecbb0bcf82890839ab7750fa662
franalvarez88 <franalvarez88@gmail.com>
2022-12-17 12:53:34 +0100

    valoraciones

--- src/components/personaje/Personaje.jsx
+++ src/components/personaje/Personaje.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import './personaje.css'
 import { Image } from 'react-bootstrap'
+import Valoraciones from '../valoraciones/Valoraciones'
+import { useTranslation } from "react-i18next";
 
 function Personaje(props) {
+  console.log("hola", props)
+  const { i18n } = useTranslation();
   const personaje = props.personaje
   return (
-    <div className='contenedor_principal'>
+    <div className='contenedor_principal d-flex flex-column'>
       <div className='contenedor_imagen'>
         <Image
           className='imagen_personaje'
           src={`${personaje.thumbnail.path}.${personaje.thumbnail.extension}`}
           alt={`${personaje.name} imagen`}
         />
+      </div>
+      <div className="contenedor_nombre_personaje">
+        <p>{personaje.name} </p>
+      </div>
+      <div className="contenedor_valoraciones">
+        <Valoraciones />
+      </div>
+      <div className="contenedor:descripcion">
+        <p>{personaje.description[i18n.language]} </p>  
+      </div>
+      <div className="boton_personaje">
+      <button type="button" className="boton_personaje"
+      >Descubrelo</button>
       </div>
     </div>
   )
