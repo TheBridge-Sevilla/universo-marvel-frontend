26380832c87f8431f159487f65b14d808fb4f536
idavid80 <dominguezbueno.david@gmail.com>
2022-12-17 14:03:17 +0100

    componentes creados sin terminar

---
+++ src/components/AvatarUsuario.jsx
@@ -0,0 +1,14 @@
+import Avatar from '@mui/material/Avatar'
+import Badge from '@mui/material/Badge'
+
+function AvatarUsuario() {
+  
+  return (
+
+    <Badge badgeContent={4}>
+      <Avatar color="action" />
+    </Badge>
+
+  )
+}
+export default AvatarUsuario
---
+++ src/components/BotonVolver.jsx
@@ -0,0 +1,24 @@
+import AvatarUsuario from './AvatarUsuario'
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft'
+import { useContextoUsuario } from '../context/contextoUsuario'
+import { useEffect, useState } from 'react'
+import Nav from 'react-bootstrap/Nav';
+
+export default function IconButtons() {
+  const { pantalla, setPantalla, usuario } = useContextoUsuario()
+  const [volver, setVolver] = useState()
+
+  const regresar = () => setPantalla(volver)
+
+  useEffect(() => {
+    setVolver(pantalla)
+  }, [pantalla])
+
+
+  return (
+    <Nav class='d-flex justify-content-between' >
+        <ArrowCircleLeftIcon onClick={regresar} />
+      <AvatarUsuario />
+    </Nav>
+  )
+}
