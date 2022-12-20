8a1aaa79e12b0b79eb558000e635406ec71580ad
franalvarez88 <franalvarez88@gmail.com>
2022-12-20 17:35:44 +0100

    maquetado css

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -2,6 +2,7 @@ import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material'
 import { useState } from 'react'
 import { ArrowCounterclockwise, Gear, StarHalf } from 'react-bootstrap-icons'
 import { useNavigate } from 'react-router-dom'
+import './bottombar.css'
 
 export default function BottomBar() {
   const [value, setValue] = useState(0)
@@ -11,25 +12,33 @@ export default function BottomBar() {
     <Paper
       sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
       elevation={3}
+      className="contenedor_barra_navegación"
     >
-      <BottomNavigation
-        showLabels
+      <BottomNavigation 
+      className='contenedor_navegacion'
+      showLabels
         value={value}
         onChange={event => {
           setValue(event.value)
         }}
       >
         <BottomNavigationAction
+      className='contenedor_boton_navegacion'
+
           label='Recents'
           icon={<ArrowCounterclockwise />}
           onClick={() => navigate('introducirDireccion')}
         />
         <BottomNavigationAction
+      className='contenedor_boton_navegacion'
+
           label='Personajes'
           icon={<StarHalf />}
           onClick={() => navigate('introducirDireccion')}
         />
         <BottomNavigationAction
+      className='contenedor_boton_navegacion'
+
           label='Ajustes'
           icon={<Gear />}
           onClick={() => navigate('/iniciar-sesion')}
