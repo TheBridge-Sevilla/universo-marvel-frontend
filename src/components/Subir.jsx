bfd6113459688a029662b8833c5bb8e959218af5
Ulyssbat <stewartbatallasminda@gmail.com>
2023-01-03 20:54:28 +0100

    elimina la importacion de react al ser innecesaria

--- src/components/Subir.jsx
+++ src/components/Subir.jsx
@@ -1,30 +1,37 @@
-import React, {useState,useEffect} from 'react';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { useState, useEffect } from 'react'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
 
 export default function Subir() {
+  const [showButton, setShowButton] = useState(false)
 
-    const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShowButton(true)
+      } else {
+        setShowButton(false)
+      }
+    }
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 100) {
-                setShowButton(true);
-            } else {
-                setShowButton(false);
-            }
-        };
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+  const handleClick = () => {
+    window.scrollTo(0, 0)
+  }
 
-    const handleClick = () => {
-        window.scrollTo(0, 0);
-    };
-
-    return (
-        <>
-        {showButton ? <ArrowUpwardIcon className='flecha-subir show' fontSize='large' onClick={handleClick}></ArrowUpwardIcon> : <></>}
-        </>
-    );
+  return (
+    <>
+      {showButton ? (
+        <ArrowUpwardIcon
+          className='flecha-subir show'
+          fontSize='large'
+          onClick={handleClick}
+        ></ArrowUpwardIcon>
+      ) : (
+        <></>
+      )}
+    </>
+  )
 }
