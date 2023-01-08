c0c9342252d859c4a4de069fab98bb694874ce85
idavid80 <dominguezbueno.david@gmail.com>
2023-01-08 19:41:07 +0100

    carrusel destacado

--- src/pages/destacado/Destacado.jsx
+++ src/pages/destacado/Destacado.jsx
@@ -5,13 +5,32 @@ import Favorito from './Favorito'
 import MasVotado from './MasVotado'
 
 export default function Destacados() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  }
   return (
     <>
       <Nav className='justify-content-end'>
         <TopBar />
       </Nav>
-      <Favorito />
-      <MasVotado/>
+      <Favorito responsive={responsive}/>
+      <MasVotado responsive={responsive}/>
       <Navbar />
     </>
   )
