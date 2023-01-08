cdc6749ca238feadd2ee67acd5f7819f9a3de920
Ulyssbat <stewartbatallasminda@gmail.com>
2023-01-08 22:26:08 +0100

    Quita el favorito para el usuario invitado

--- src/pages/destacado/Destacado.jsx
+++ src/pages/destacado/Destacado.jsx
@@ -3,10 +3,8 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from '../../components/Navbar'
 import Favorito from './Favorito'
 import MasVotado from './MasVotado'
-import { useContextoUsuario } from '../../context/contextoUsuario'
 
 export default function Destacados() {
-  const { usuario } = useContextoUsuario()
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
